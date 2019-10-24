30133a004d919823f66ff83c61966096a6b98bd8
redian23 <vasya.degski@gmail.com>
2019-10-24 20:43:07 +0300

    Upload on G.H

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        StartClass startClass = new StartClass();
+        startClass.start();
+        startClass.activation();
+        startClass.stop();
+    }
+}
---
+++ src/StartClass.java
@@ -0,0 +1,14 @@
+public class StartClass {
+
+    public void start(){
+        System.out.println("Start");
+    }
+
+    public void stop(){
+        System.out.println("Stop");
+    }
+
+    public void activation (){
+        System.out.println("Write Console...");
+    }
+}
