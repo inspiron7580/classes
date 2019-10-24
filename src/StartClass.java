ba5dd602f3a031132cda6b176566ecf7a6160a27
Вася Климов <46902231+redian23@users.noreply.github.com>
2019-10-24 21:01:02 +0300

    Add "for" on StartClass

--- src/StartClass.java
+++ src/StartClass.java
@@ -9,6 +9,14 @@ public class StartClass {
     }
 
     public void activation (){
+        for (int i; i<4;i++){
+        System.out.println("Write Console.");
+             for (int j; j<4;j++){
+        System.out.println("Write Console..");
+                 for (int k; k<4;k++){
         System.out.println("Write Console...");
+                 }
+            }
+        }
     }
 }
